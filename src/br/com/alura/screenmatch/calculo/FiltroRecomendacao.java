0c8e3079cef5265c8dab0668220a9442a46fb37a
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-19 19:47:03 -0300

    Class FiltroRecomendacao

--- src/br/com/alura/screenmatch/calculo/FiltroRecomendacao.java
+++ src/br/com/alura/screenmatch/calculo/FiltroRecomendacao.java
@@ -4,9 +4,9 @@ public class FiltroRecomendacao {
 
     public void filtra(Classificavel classificavel){
         if (classificavel.getClassificacao() >= 4){
-            System.out.println("Está entre os preferidos do momento");
+            System.out.println("Está entre os preferidos do momento!");
         } else if (classificavel.getClassificacao() >= 2) {
-            System.out.println("Muito bem avaliado no momento");
+            System.out.println("Muito bem avaliado no momento!");
         } else {
             System.out.println("Coloque na sua lista para assistir depois");
         }

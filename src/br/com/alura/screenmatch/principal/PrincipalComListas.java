3e1b438855aa2f1bda74eecaa6ad95468daea5a3
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-20 20:04:09 -0300

    commit Collections

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -7,6 +7,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     static void main(String[] args) {
@@ -42,6 +43,9 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtista);
         System.out.println("Lista de titulos ordenados");
         Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
         System.out.println(lista);
 
     }

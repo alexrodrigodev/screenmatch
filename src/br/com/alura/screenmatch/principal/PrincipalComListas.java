cef2b1e200554492dd0e3df209b461887fe9a469
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-20 18:39:54 -0300

    commit instanceoff

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -24,7 +24,9 @@ public class PrincipalComListas {
         lista.add(lost);
         for (Titulo item: lista) {
             System.out.println(item.getNome());
-
+            if (item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
         }
     }
 }

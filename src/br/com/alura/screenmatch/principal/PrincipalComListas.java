6f5eed6767d4084f60aad3c9008e2f2eead2cba8
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-20 18:35:17 -0300

    commit novo pacote principal

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -9,9 +9,13 @@ import java.util.ArrayList;
 public class PrincipalComListas {
     static void main(String[] args) {
         Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
         Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
         var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
         Serie lost = new Serie("Lost", 2000);
+
 
         ArrayList<Titulo> lista = new ArrayList<>();
         lista.add(filmeDoPaulo);
@@ -19,7 +23,8 @@ public class PrincipalComListas {
         lista.add(outroFilme);
         lista.add(lost);
         for (Titulo item: lista) {
-            System.out.println(item);
+            System.out.println(item.getNome());
+
         }
     }
 }

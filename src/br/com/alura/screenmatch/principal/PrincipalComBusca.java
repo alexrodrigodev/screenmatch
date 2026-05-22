6522b49acf40c3a055d513c790891a162e8a2353
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-22 20:29:40 -0300

    commit gson upper camel case

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -32,8 +32,10 @@ public class PrincipalComBusca {
         System.out.println(json);
 
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        //Titulo meuTitulo = gson.fromJson(json, Titulo.class);
         TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println("Titulo convertido: ");
+        System.out.println(meuTitulo);
     }
 }

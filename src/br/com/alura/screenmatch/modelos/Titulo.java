f22708ca467f029416f961c5f1907befe47d4ea0
Alex Rodrigo <alexrodrigodev@gmail.com>
2026-05-23 19:22:46 -0300

    commit FileWriter

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -87,7 +87,7 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento + "," + " Duração=" + duracaoEmMinutos;
+        return "(nome = " + nome +
+                ", anoDeLancamento =" + anoDeLancamento + "," + " Duração = " + duracaoEmMinutos + ")";
     }
 }
